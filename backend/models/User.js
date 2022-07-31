964dd151ff596b77b53b2f027feb164ad79d10bf
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-07-31 21:22:29 +0600

    UserSchema and Server.js fix

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,13 @@
-const { Schema, default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new Schema(
+const UserSchema = mongoose.Schema(
   {
     name: {
       type: String,
       required: [true, "is required"],
     },
+
     email: {
       type: String,
       required: [true, "is required"],
@@ -14,19 +15,22 @@ const userSchema = new Schema(
       index: true,
       validate: {
         validator: function (str) {
-          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(str);
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(str);
         },
-        message: (props) => `${props.value} is not a valid email address`,
+        message: (props) => `${props.value} is not a valid email`,
       },
     },
+
     password: {
       type: String,
       required: [true, "is required"],
     },
+
     isAdmin: {
       type: Boolean,
       default: false,
     },
+
     cart: {
       type: Object,
       default: {
@@ -34,26 +38,23 @@ const userSchema = new Schema(
         count: 0,
       },
     },
+
     notifications: {
       type: Array,
       default: [],
     },
-    orders: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Order",
-      },
-    ],
+
+    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
   },
-  { minimize: false, timestamps: true }
+  { minimize: false }
 );
 
-UserSchema.statics.findByCredentials = async function ({ email, password }) {
+UserSchema.statics.findByCredentials = async function (email, password) {
   const user = await User.findOne({ email });
-  if (!user) throw new Error("Invalid email or password");
+  if (!user) throw new Error("invalid credentials");
   const isSamePassword = bcrypt.compareSync(password, user.password);
   if (isSamePassword) return user;
-  throw new Error("Invalid email or password");
+  throw new Error("invalid credentials");
 };
 
 UserSchema.methods.toJSON = function () {
@@ -63,10 +64,12 @@ UserSchema.methods.toJSON = function () {
   return userObject;
 };
 
-// before saving => hash the password
+//before saving => hash the password
 UserSchema.pre("save", function (next) {
   const user = this;
+
   if (!user.isModified("password")) return next();
+
   bcrypt.genSalt(10, function (err, salt) {
     if (err) return next(err);
 
@@ -83,6 +86,6 @@ UserSchema.pre("remove", function (next) {
   this.model("Order").remove({ owner: this._id }, next);
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,13 @@ const io = new Server(server, {
   methods: "*",
 });
 
+const User = require("./models/User");
+const userRoutes = require("./routes/userRoutes");
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/users", userRoutes);
 
 server.listen(8080, () => {
   console.log("server running at", 8080);
