169c35d047159688722985a92525125b13e8cc43
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-07-31 17:55:21 +0600

    Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <Navigation />
         <Routes>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,62 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import categories from "../categories";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <img
+        src="https://res.cloudinary.com/learn-code-10/image/upload/v1653947013/yqajnhqf7usk56zkwqi5.png"
+        alt="Home Banner"
+      />
+      <div className="featured__products--container container mt-4">
+        <h2>Last Products</h2>
+        {/* last products here*/}
+        <div>
+          <Link
+            to="/category/all"
+            style={{
+              textAlign: "right",
+              display: "block",
+              textDecoration: "none",
+            }}
+          >
+            See more {">>"}
+          </Link>
+        </div>
+      </div>
+      {/* sale banner */}
+      <div className="sale__banner--container mt-4">
+        <img
+          src="https://res.cloudinary.com/learn-code-10/image/upload/v1654093280/xkia6f13xxlk5xvvb5ed.png"
+          alt="Sale Banner"
+        />
+      </div>
+      <div className="recent__products--container container mt-4">
+        <h2>Categories</h2>
+        <Row>
+          {categories.map((category) => (
+            <LinkContainer to={`/category/${category.name.toLocaleLowerCase}`}>
+              <Col md={4}>
+                <div
+                  style={{
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${category.img})`,
+                    gap: "10px",
+                  }}
+                  className="category__title"
+                >
+                  {category.name}
+                </div>
+              </Col>
+            </LinkContainer>
+          ))}
+        </Row>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
